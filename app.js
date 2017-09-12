07fd70be14427956458734ed9e2bbb7ebf639ab0
Rdyx <tiranico.pro@gmail.com>
2017-09-12 19:21:10 +0200

    add function creation

--- app.js
+++ app.js
@@ -1,56 +1,16 @@
-// var col = 10;
-// var row = 10;
-// $('#content').empty();
-// var ligne;
-// var array = [];
-// var FN = 1;
-// var FR = 2;
-
-// for (var i = 0; i < col; i++) {
-//     ligne += '<td data-y="' + i + '">' + i + '</td>';
-//     array.push(Array(10).fill(0))
-// }
-
-// for (var i = 0; i < row; i++) {
-//     $('#content').append('<tr data-x="' + i + '">' + ligne + '</tr>');
-// }
-
-// // setInterval(function random(min, max){
-// //     min = Math.ceil(0);
-// //     max = Math.floor(10);
-// //     array[Math.floor(Math.random() * (max - min)) + min][Math.floor(Math.random() * (max - min)) + min] = 1;
-// //     console.log(array);
-// //     for(var i = 0; i < array.length; i++){
-// //         for (var j = 0; j < array[i].length; j++) {
-// //             console.log(array[i][j])
-// //             if(array[i][j] == FN){
-// //                 return array;
-// //             }
-
-// //         }
-// //     }
-// // }, 1000);
-
-// console.log(array.length)
-
-// function getRandomInt(min, max) {
-//     min = Math.ceil(min);
-//     max = Math.floor(max);
-//     return Math.floor(Math.random() * (max - min)) + min;
-//   }
-
-
 var tableau = []
-var row = 10;
-var col = 10;
+var min = 0;
+var max = 100;
+var FN = 1;
+var FR = 2;
 var html;
-var randomz = Math.floor(Math.random() * (Math.floor(100) - Math.ceil(0))) + Math.ceil(0);
+var i = 0;
+//var randomz = Math.floor(Math.random() * (Math.floor(100) - Math.ceil(0))) + Math.ceil(0);
 
-for (var i = 0; i < row; i++) {
+for (var i = 0; i < 10; i++) {
     html += '<tr>'
-    for (var j = 0; j < col; j++) {
+    for (var j = 0; j < 10; j++) {
         html += '<td id="num' + i + '' + j + '">0</td>';
-        tableau.push(i + "" + j)
     }
     html += '</tr>'
 }
@@ -69,14 +29,45 @@ $('#content').html(html);
 //     }
 // }, 500);
 
-// function creation(nombre){
-//     for(var i = 0; i < nombre; i++){
-//         console.log(i)
-//     }
-// }
+//Fonction pour créer les fourmis dans le tableau en prenant un nombre et la couleur en paramètre
+function creation(nombre, couleur) {
+    //On initialise j à 0
+    var j = 0;
+    //On set le random min
+    var min = 0;
+    //Le random max
+    var max = 100;
+    //On lance la boucle, tant que j est < à nombre, la boucle s'excécute
+    while (j < nombre) {
+        //On insert une valeur random dans randomz à chaque début de boucle
+        randomz = (Math.floor(Math.random() * (max - min)) + min);
+        //Si randomz < à 10, il nous faut spécifier un id qui commence par #num0 (01, 02...)
+        if (randomz < 10) {
+            //Si num0x est égal à 0, on push le param couleur dedans et on 
+            //incrément j pour diminuer le nombre de tours de boucle
+            if ($('#num0' + randomz).html() == 0) {
+                $('#num0' + randomz).html(couleur);
+                j++;
+            } 
+            //Si la case est déjà pleine, on relance la fonction sans changer ses paramètres
+            else {
+                creation();
+            }
+        //Si randomz > 10, on push la couleur dans une case #num
+        //Le reste du code est le même que pour randomz < 10
+        } else {
+            if ($('#num' + randomz).html() == 0) {
+                $('#num' + randomz).html(couleur);
+                j++;
+            } else {
+                creation();
+            }
+        }
+    }
+}
 
-// creation(2)
-
+creation(5, 'rouge');
+creation(5, 'noir');
 // function getRandomInt(min, max) {
 //     min = Math.ceil(min);
 //     max = Math.floor(max);
